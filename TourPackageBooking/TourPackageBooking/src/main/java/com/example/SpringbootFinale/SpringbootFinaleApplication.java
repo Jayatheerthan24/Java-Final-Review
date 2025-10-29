da38cbbd1c505f04b4a27d32e61dfebcea7838ed
Jayatheerthan24 <your.email@example.com>
2025-10-29 09:43:43 +0530

    Initialised the Tour Package Booking with necessary dependancies .

---
+++ TourPackageBooking/TourPackageBooking/src/main/java/com/example/SpringbootFinale/SpringbootFinaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootFinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFinaleApplication.class, args);
+	}
+
+}
---
+++ TourPackageBooking/TourPackageBooking/src/test/java/com/example/SpringbootFinale/SpringbootFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootFinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
